693a0aaa002a5ed819a80286f3f2e9c7b92ddcff
REVANPL <janek2000-2000@wp.pl>
2021-05-14 12:54:00 +0200

    Tested refactred function on device

--- GwinstekLCRTester/RSCommunication.cs
+++ GwinstekLCRTester/RSCommunication.cs
@@ -15,14 +15,16 @@ namespace GwinstekLCRTester
         private static SerialPort _serialPort;
         private static TextWriter writer;
 
-        public static readonly string[] measurementTypes = { ///0 is 0xE9
+        public static readonly string[] measurementTypes = {
 
-            "Cs-Rs", "Cs-D", "Cp-Rp",
-            "Cp-D","Lp-Rp", "Lp-Q", "Ls-Rs",
-            "Ls-Q", "Rs-Q", "Rp-Q",
-            "R-X", "DCR", "Z-0r","Z-0d",
+            "Cs-Rs", "Cs-D", 
+            "Cp-Rp", "Cp-D",
+            "Lp-Rp", "Lp-Q", 
+            "Ls-Rs", "Ls-Q", 
+            "Rs-Q", "Rp-Q", 
+            "R-X", "DCR", 
+            "Z-0r","Z-0d",
             "Z-D", "Z-Q"
-
         };
 
         public RSCommunication(string portName, uint baudRate, Parity parityNumber, uint dataBits, StopBits stopBits, Handshake handshakeType, int readTimeout = 30000, int writeTimeout = 30000)
@@ -34,32 +36,34 @@ namespace GwinstekLCRTester
             _serialPort.BaudRate = (int)baudRate;
             _serialPort.ReadTimeout = readTimeout;
             _serialPort.WriteTimeout = writeTimeout;
-
-            try
-            {
-                _serialPort.Handshake = handshakeType;
-                _serialPort.Parity = parityNumber;
-                _serialPort.StopBits = stopBits;
-            }
-            catch (ArgumentOutOfRangeException e) { Console.WriteLine(e.Message); }
-
+            _serialPort.Handshake = handshakeType;
+            _serialPort.Parity = parityNumber;
+            _serialPort.StopBits = stopBits;
             _serialPort.RtsEnable = true;
-            if (_serialPort.IsOpen) _serialPort.Close();
+
+            if (_serialPort.IsOpen) 
+                _serialPort.Close();
+
             _serialPort.Open();
-            if (!_serialPort.IsOpen) throw new Exception("Nie udało otworzyć się portu o takich parametrach");
+
+            if (!_serialPort.IsOpen) 
+                throw new Exception("Nie udało otworzyć się portu o takich parametrach");
+
             _serialPort.WriteLine("SYST:CODE OFF");
             _serialPort.WriteLine("DISP:PAGE meas");
         }
 
 
         // TODO: przenieś to do FileHandlera
-        public void writeToCSV(decimal[] paramArray, string multiplier, string freq, string msType, string pathOutput, int cyclesIterator, uint avg=1)
+        public void writeToCSV(decimal[] paramArray, string multiplier, string freq, string msType, int cyclesIterator, uint avg=1)
         {
+
+            // pathOutput zmienić na jakis atrybut kiedy przniesie się tą metodę do fileHandlera.cs
             string path = pathOutput.Replace(@"\", @"\\").Replace("\r\n", "") + "\\pomiary_" + DateTime.Now.ToString("dd-M-yyyy--HH-mm-ss") + ".csv";
 
             if (writer == null)
             {
-
+                // tworzenie kolumn do pliku csv
                 writer = File.AppendText(path);
                 string csvColumns = "Numer cyklu; AVG;";
                 csvColumns += msType switch
@@ -89,22 +93,42 @@ namespace GwinstekLCRTester
 
             if (msType != "DCR")
             {
-                // 0 to numer cyklu, 1 to AVG,  2 i 3 paramArray, 4 to D, 5 to freq, 6 to data
-                writer.WriteLine("{0};{1};{2};{3};{4};{5};{6}", cyclesIterator, (avg!=1) ? avg : "NIE",  paramArray[0].ToString().Replace(",", "."), paramArray[1].ToString().Replace(",", "."), (paramArray[2] == -1) ? "__" : paramArray[2].ToString().Replace(",", "."), freq, DateTime.Now.ToString("dd-M-yyyy HH:mm:ss"));
+                writer.WriteLine
+                (
+                    "{0};{1};{2};{3};{4};{5};{6}", 
+                    cyclesIterator,                                                                 // numer kondensatora
+                    (avg!=1) ? avg : "NIE",                                                         // mierzenie z parametrem avg
+                    paramArray[0].ToString().Replace(",", "."),                                     // główny parametr pomiaru
+                    paramArray[1].ToString().Replace(",", "."),                                     // drugi główny parametr pomiaru                       
+                    (paramArray[2] != -1) ? paramArray[2].ToString().Replace(",", ".") : "NIE",     // dodatkowy parametr D
+                    freq,                                                                           // częstotliwość pomiaru
+                    DateTime.Now.ToString("dd-M-yyyy HH:mm:ss")                                     // czas pomiaru
+                );
             }
             else
             {
-                writer.WriteLine("{0};{1};{2};{3};{4};{5}", cyclesIterator, (avg != 1) ? avg : "NIE", paramArray[0] * 0.001m, (paramArray[2] == -1) ? "__" : paramArray[2].ToString().Replace(",", "."), freq, DateTime.Now.ToString("dd-M-yyyy HH:mm:ss"));
+                writer.WriteLine
+                (
+                    "{0};{1};{2};{3};{4};{5}", 
+                    cyclesIterator,                                                                 // numer kondenstatora
+                    (avg != 1) ? avg : "NIE",                                                       // mierzenie z parametrem avg
+                    paramArray[0] * 0.001m,                                                         // główny i jedyny dla DCR parametr pomiaru
+                    (paramArray[2] == -1) ? "NIE" : paramArray[2].ToString().Replace(",", "."),     // dodatkowy parametr D
+                    freq,                                                                           // częstotliwość pomiaru
+                    DateTime.Now.ToString("dd-M-yyyy HH:mm:ss")                                     // czas pomiaru
+                );
             }
         }
 
 
-        public decimal[] getMeasurementParams(string msType, string multiplier, bool addD = false, int waitFetchMs = 0)
+        public decimal[] getMeasurementParams(string msType, string multiplier, bool addD = false, int waitFetchMs = 500)
         {
 
             string d_Parameter = null;
             decimal[] responseDecimalArray = new decimal[3];
 
+
+            // dodatkowe opcjonalne mierzenie parametru D
             if (addD)
             {
                 setMeasurementInDevice("Cs-D");
@@ -113,6 +137,7 @@ namespace GwinstekLCRTester
                 d_Parameter = _serialPort.ReadLine().Split(",")[1].Replace(".", ",");
             }
 
+            // mierzenie głównych parametrów
             setMeasurementInDevice(msType);
             System.Threading.Thread.Sleep(waitFetchMs);
             _serialPort.WriteLine("FETCH?");
@@ -168,13 +193,16 @@ namespace GwinstekLCRTester
 
             }*/
 
-            // jeżeli mierzymy farady lub henry to możemy dodawać do nich mnożniki
+            // dodawanie mnożników, jeśli jednostka pomiaru pozwala na to
             // dodatkowe zabezpieczenie oprócz GUI
             if (msType.Contains("Cs") || msType.Contains("Cp") || msType.Contains("Ls") || msType.Contains("Lp"))
             {
                 responseDecimalArray[0] = applyMulitplier(responseDecimalArray[0], multiplier);
             }
 
+            // element [0] to główny parametr pomiaru, nigdy nie powinien być nullem lub -1
+            // element [1] to drugi główny parametr pomiaru, w przypadku trybu DCR jest równy -1
+            // element [2] to opcjonalny parametr D, gdy użytkownik go nie podał jest równy -1
             return responseDecimalArray;
         }
 
@@ -193,7 +221,9 @@ namespace GwinstekLCRTester
         }
 
 
-        /* dla liczb, które po przemnożeniu przez 1000 nadal mają liczby po przecinku funkcja je zaokrągla*/
+
+        // funkcja zmieniająca częstotliwość w urządzeniu
+        // UWAGA : liczby, które po przemnożeniu przez 1000 nadal są zmiennoprzecinkowe są zaokrąglane
         public void changeHzInDevice(string HzString)
         {
             int Hz = -1;
